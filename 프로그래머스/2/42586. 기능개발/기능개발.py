c542154b746200e64470a4e5e371026e4d4cbba6
박정원 <126760637+p-garden@users.noreply.github.com>
2025-07-11 15:02:51 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,13 @@
-import math
+'''
+모범답안 -(p-100)//s -> ceil사용안하고 올림 연산 가능
+Q[-1][0]에 남은 일자 계산 / [-1][1]를 로 사용
+'''
+
 def solution(progresses, speeds):
-    answer = []
-    days=[]
-    for p,s in zip(progresses,speeds):
-        remain = math.ceil((100 - p) / s)
-        days.append(remain)
-    cnt=1
-    comp = days[0]
-    
-    for day in days[1:]:
-        if day <= comp:
-            cnt+=1
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
         else:
-            answer.append(cnt)
-            cnt=1
-            comp=day
-    answer.append(cnt)
-    return answer
+            Q[-1][1]+=1
+    return [q[1] for q in Q]
