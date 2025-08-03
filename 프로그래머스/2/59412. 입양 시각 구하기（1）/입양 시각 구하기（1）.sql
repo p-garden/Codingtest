7b5550ce66224198442ce74923d2550d7a9d853c
박정원 <126760637+p-garden@users.noreply.github.com>
2025-08-03 10:42:58 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT hour(DATETIME) as 'HOUR', count(*) as 'COUNT'
+    from ANIMAL_OUTS
+    where hour(DATETIME) between 9 and 20
+    group by hour(DATETIME)
+    order by hour(DATETIME) asc;
