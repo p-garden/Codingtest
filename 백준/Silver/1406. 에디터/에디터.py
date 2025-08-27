9ab0314400b49815b662c30a2b8f2aaadc9d13d1
박정원 <126760637+p-garden@users.noreply.github.com>
2025-08-27 17:40:05 +0900

    [Silver II] Title: 에디터, Time: 228 ms, Memory: 42832 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+left = list(input().rstrip())
+right = list()
+M = int(input())
+
+for _ in range(M):
+  cmd = input().split()
+  if cmd[0] == 'L':
+    if left:
+      right.append(left.pop())
+  elif cmd[0] =='D':
+    if right:
+      left.append(right.pop())
+  elif cmd[0] =='B':
+    if left:
+      left.pop()
+  else:
+    left.append(cmd[1])
+print(''.join(left + right[::-1]))
