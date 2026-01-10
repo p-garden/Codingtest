357ce2e917f895e5e74bc1e0f56b6f5e8beab813
박정원 <126760637+p-garden@users.noreply.github.com>
2026-01-10 13:53:44 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,11 +1,4 @@
--- 코드를 입력하세요
-SELECT concat('/home/grep/src/',
-              b.BOARD_ID, '/',
-              g.FILE_ID, 
-              g.FILE_NAME,
-              g.FILE_EXT) FILE_PATH
-    from USED_GOODS_BOARD b
-        inner join USED_GOODS_FILE g
-        on b.BOARD_ID = g.BOARD_ID
-    where b.VIEWS = (select max(VIEWS) from USED_GOODS_BOARD)
-    order by  g.FILE_ID desc;
+select concat('/home/grep/src/', a.BOARD_ID, '/', b.FILE_ID, b.FILE_NAME, b.FILE_EXT) FILE_PATH
+    from USED_GOODS_BOARD a join USED_GOODS_FILE b on a.BOARD_ID = b.BOARD_ID
+    where a.VIEWS  = (select max(VIEWS) from USED_GOODS_BOARD)
+    order by b.FILE_ID desc ;
