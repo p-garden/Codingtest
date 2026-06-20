e520769e84497bda282417ddb910941855d95ee0
p-garden <126760637+p-garden@users.noreply.github.com>
2026-06-20 15:15:22 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME
+select ANIMAL_ID,  NAME
     from ANIMAL_INS
-    where NAME like '%el%' and ANIMAL_TYPE = 'Dog'
-    order by NAME;
+    where NAME LIKE lower('%el%') and ANIMAL_TYPE = 'Dog'
+    order by NAME, ANIMAL_ID;
