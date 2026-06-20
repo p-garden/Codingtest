bdb0d7bd809b6fe2b550d701e6ae522382609ad7
p-garden <126760637+p-garden@users.noreply.github.com>
2026-06-20 15:30:50 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,5 +1,5 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME, 
-    case when (SEX_UPON_INTAKE REGEXP ('Neutered|Spayed')) THEN 'O'
-    else  'X' end as 중성화
-    from ANIMAL_INS;
+select ANIMAL_ID, NAME, 
+    case when SEX_UPON_INTAKE like ('%Neutered%') or SEX_UPON_INTAKE like ('%Spayed%') then 'O' 
+    else 'X' end as 중성화
+    from ANIMAL_INS
+    order by ANIMAL_ID ;
