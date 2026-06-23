03a23ae15ca26a0c42d2200828d502661efdb55f
p-garden <126760637+p-garden@users.noreply.github.com>
2026-06-23 22:46:39 +0900

    [level 3] Title: 부서별 평균 연봉 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
+++ 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
@@ -1,9 +1,6 @@
--- 코드를 작성해주세요
-select d.DEPT_ID as 'DEPT_ID',
-     d.DEPT_NAME_EN as 'DEPT_NAME_EN',
-    round(avg(e.SAL),0) as AVG_SAL
-    from HR_EMPLOYEES e
-        inner join HR_DEPARTMENT d
-        on e.DEPT_ID = d.DEPT_ID
+select d.DEPT_ID, d.DEPT_NAME_EN, round(avg(e.SAL),0) AVG_SAL
+    from HR_DEPARTMENT d 
+        inner join HR_EMPLOYEES e
+        on d.DEPT_ID = e.DEPT_ID
     group by d.DEPT_ID
-    order by avg(e.SAL) desc
+    order by AVG_SAL desc;
