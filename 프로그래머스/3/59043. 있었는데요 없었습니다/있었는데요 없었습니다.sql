3d0b209636a3894cd26e957b2e0031044746a487
p-garden <126760637+p-garden@users.noreply.github.com>
2026-06-23 23:17:54 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,7 +1,6 @@
--- 코드를 입력하세요
-SELECT I.ANIMAL_ID, I.NAME
-    from  ANIMAL_INS I
-        inner join ANIMAL_OUTS O
-        on I.ANIMAL_ID = O.ANIMAL_ID
-    where O.DATETIME < I.DATETIME
-    order by I.DATETIME
+select i.ANIMAL_ID, i.NAME
+    from ANIMAL_INS i 
+        join ANIMAL_OUTS o
+        on i.ANIMAL_ID = o.ANIMAL_ID
+    where o.DATETIME < i.DATETIME
+    order by i.DATETIME;
