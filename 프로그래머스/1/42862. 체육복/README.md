b8727411eb067b4297d81343c08af3bebb8460f5
p-garden <126760637+p-garden@users.noreply.github.com>
2026-09-08 14:08:00 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 11.4 MB, 시간: 0.01 ms
+메모리: 11.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 15일 17:22:54
+2026년 09월 08일 14:07:56
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,24 +1,18 @@
 def solution(n, lost, reserve):
-    
+    #lost와 reserve의 중복 학생 제거 및 각 리스트에서 중복 경우 없으니 집합 사용
     lost = set(lost)
     reserve = set(reserve)
-    
-    diff = lost & reserve
+    diff = reserve & lost
     lost -= diff
     reserve -= diff
     
-    answer = n - len(lost)
-    
+    answer = n-len(lost)
     for i in sorted(lost):
-        sub1 = i - 1
-        sub2 = i + 1
-        if sub1 in reserve:
-            lost.remove(i)
-            reserve.remove(sub1)
+        if (i-1) in reserve:
+            reserve.remove(i-1)
             answer += 1
-        elif sub2 in reserve:
-            lost.remove(i)
-            reserve.remove(sub2)
-            answer += 1
-            
+        elif (i+1) in reserve:
+            reserve.remove(i+1)
+            answer+=1
     return answer
+
