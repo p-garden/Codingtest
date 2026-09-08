d2468fce6433e68175d16edbc50eaafd44e78b98
p-garden <126760637+p-garden@users.noreply.github.com>
2026-09-08 14:58:11 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,19 +1,16 @@
 def solution(name):
-    answer = 0
-    
-    #알파벳 이동
+    #각 위치에서 알파벳 이동 횟수는 고정+ A에따른 커서 이동만 변수,name 순회하며 A전 위치에서 A스킵하는 두가지 방법을 고려해 최소 이동 값을 갱신
+    alpha =0
     for i in name:
-        answer += min(ord(i) - ord('A'),  ord('Z')-ord(i)+1)
+        alpha += min(ord(i)-ord('A'), ord('Z')-ord(i)+1)
     
-    #커서 이동
-    n_len = len(name)
-    min_move = n_len-1
-    
-    for i in range(n_len):
-        next_idx = i+1
-        while next_idx < n_len and name[next_idx] == 'A':
-            next_idx += 1
-            
-        min_move = min(min_move, i*2+(n_len-next_idx), (n_len-next_idx)*2+i)
+    n = len(name)
+    cursor= n-1
+    for i in range(n):
+        next_idx =i+1
+        while next_idx < n and name[next_idx] =='A':
+            next_idx +=1
+        cursor = min(cursor, i*2+n-next_idx)
+        cursor = min(cursor, (n-next_idx)*2+i)
         
-    return answer + min_move
+    return alpha+cursor
