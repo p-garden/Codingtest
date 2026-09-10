4d9889ed8e57dcb93265fa8a81ac2866ba0663a1
p-garden <126760637+p-garden@users.noreply.github.com>
2026-09-10 21:21:32 +0900

    [level 3] Title: 숫자 게임, Time: 47.62 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -1,16 +1,16 @@
 def solution(A, B):
-    B.sort(reverse=True)
-    A.sort(reverse=True)
+    answer = 0
     A_point=0
     B_point=0
-    n = len(B)
-    answer = 0
-    while A_point < n: 
-        if A[A_point] < B[B_point]:
-            answer+=1
-            A_point+=1
+    A.sort(reverse=True)
+    B.sort(reverse=True)
+    n=len(A)
+    while A_point<n:
+        if B[B_point]>A[A_point]:
             B_point+=1
+            A_point+=1
+            answer+=1
         else:
             A_point+=1
-            
+        
     return answer
